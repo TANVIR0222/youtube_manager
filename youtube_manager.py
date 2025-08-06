ea19e63a64da5d0a1da2376a810e9d2b8a277b92
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2025-08-06 23:55:20 +0600

    updated

--- youtube_manager.py
+++ youtube_manager.py
@@ -1,20 +1,40 @@
 
 def list_all_video(video):
     pass
+def add_video(video):
+    pass
+def update_video_details(video):
+    pass
+def delete_video(video):
+    pass
+def close_add(video):
+    pass
 
 
-video = []
+def main():
+    video = []
 
-while True:
-    print('\n Youtube Manager | choose an option')
-    print('1. List all youtube video ')
-    print('2. Add a youtube video ')
-    print('3. Update a youtube video details ')
-    print('4. Deleted a youtube video')
-    print('5. Exit the app ')
-    # user input 
-    choose = input("Enter your choose : ")
+    while True:
+        print('\n Youtube Manager | choose an option')
+        print('1. List all youtube video ')
+        print('2. Add a youtube video ')
+        print('3. Update a youtube video details ')
+        print('4. Deleted a youtube video')
+        print('5. Exit the app ')
+        # user input 
+        choose = input("Enter your choose : ")
 
-    match choose:
-        case 1:
-            list_all_video(video)
+        match choose:
+            case 1:
+                list_all_video(video)
+            case 2:
+                add_video(video)
+            case 3:
+                update_video_details(video)
+            case 4:
+                delete_video(video)
+            case 5:
+                close_add(video)
+                break
+            case _:
+                print('you not choose an options ')
